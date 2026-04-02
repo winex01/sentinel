a964505db41bed705e65e7b137f22bf6a3a09b8e
Winnie A. Damayo <winnie131212592@gmail.com>
2026-04-02 16:16:20 +0800

    first commit

---
+++ src/SentinelServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Winex\Sentinel;
+
+use Illuminate\Support\ServiceProvider;
+
+class SentinelServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
